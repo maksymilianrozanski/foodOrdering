549ea44311e802bab11349ab9aa51e4ed4344833
Max <maksymilianrozanski@gmail.com>
2017-10-03 21:50:12 +0200

    add choosing between drink and lunch

--- src/main/java/com/example/food/Main.java
+++ src/main/java/com/example/food/Main.java
@@ -5,27 +5,44 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main(String[] args) {
 
         // create session factory
-        SessionFactory factory = new Configuration()
-                .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(Dish.class)
-                .buildSessionFactory();
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Dish.class).buildSessionFactory();
 
-        // create session
         Session session;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter 1 to order a drink, 2 to order a lunch.");
+        int enteredNumber = scanner.nextInt();
+        switch (enteredNumber) {
+            case 1:
+                System.out.println("Entered number: " + enteredNumber);
+                System.out.println("Ordering a drink");
+                break;
+            case 2:
+                System.out.println("Entered number: " + enteredNumber);
+                System.out.println("Ordering a lunch");
+                break;
+            default:
+                System.out.println("Incorrect input, Enter 1 to order a drink, 2 to order a lunch.");
+                break;
+        }
+
 
         try {
             session = factory.getCurrentSession();
             session.beginTransaction();
             Dish dishFromDb = (Dish) session.get(Dish.class, 6);
-            System.out.println("Dish id: " + dishFromDb.getId() + " dishname: " + dishFromDb.getDishName() );
+            System.out.println("Dish id: " + dishFromDb.getId() + " dishname: " + dishFromDb.getDishName());
             session.getTransaction().commit();
-        }finally {
+        } finally {
             factory.close();
         }
     }
