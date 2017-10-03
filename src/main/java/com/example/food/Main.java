134d06ff2e127c61b5e2f7d427960e628ea989eb
Max <maksymilianrozanski@gmail.com>
2017-10-03 22:02:15 +0200

    wrap choosing drink/lunch into while loop

--- src/main/java/com/example/food/Main.java
+++ src/main/java/com/example/food/Main.java
@@ -1,10 +1,5 @@
 package com.example.food;
 
-import com.example.food.model.Dish;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
 import java.util.Scanner;
 
 public class Main {
@@ -12,38 +7,39 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // create session factory
-        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Dish.class).buildSessionFactory();
-
-        Session session;
-
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter 1 to order a drink, 2 to order a lunch.");
-        int enteredNumber = scanner.nextInt();
-        switch (enteredNumber) {
-            case 1:
-                System.out.println("Entered number: " + enteredNumber);
-                System.out.println("Ordering a drink");
-                break;
-            case 2:
-                System.out.println("Entered number: " + enteredNumber);
-                System.out.println("Ordering a lunch");
-                break;
-            default:
-                System.out.println("Incorrect input, Enter 1 to order a drink, 2 to order a lunch.");
-                break;
+//TODO: catch java.util.InputMismatchException (if entered not number)
+        while (true) {
+            System.out.println("Enter 1 to order a drink, 2 to order a lunch.");
+            int enteredNumber = scanner.nextInt();
+            switch (enteredNumber) {
+                case 1:
+                    System.out.println("Entered number: " + enteredNumber);
+                    System.out.println("Ordering a drink");
+                    break;
+                case 2:
+                    System.out.println("Entered number: " + enteredNumber);
+                    System.out.println("Ordering a lunch");
+                    break;
+                default:
+                    System.out.println("Incorrect input, Enter 1 to order a drink, 2 to order a lunch.");
+                    break;
+            }
         }
 
+//        // create session factory
+//        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Dish.class).buildSessionFactory();
+//
+//        Session session;
 
-        try {
-            session = factory.getCurrentSession();
-            session.beginTransaction();
-            Dish dishFromDb = (Dish) session.get(Dish.class, 6);
-            System.out.println("Dish id: " + dishFromDb.getId() + " dishname: " + dishFromDb.getDishName());
-            session.getTransaction().commit();
-        } finally {
-            factory.close();
-        }
+//        try {
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+//            Dish dishFromDb = (Dish) session.get(Dish.class, 6);
+//            System.out.println("Dish id: " + dishFromDb.getId() + " dishname: " + dishFromDb.getDishName());
+//            session.getTransaction().commit();
+//        } finally {
+//            factory.close();
+//        }
     }
 }
