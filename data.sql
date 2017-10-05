f636fb1c3ed9d83fb8a828b57b7f76da76d85e16
Max <maksymilianrozanski@gmail.com>
2017-10-05 21:09:48 +0200

    add sql table schema, and table data to git

---
+++ data.sql
@@ -0,0 +1,27 @@
+﻿/*
+-- Query: SELECT * FROM food.dishes
+LIMIT 0, 1000
+
+-- Date: 2017-10-05 21:02
+*/
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (5,'Kurczak','Polish','mainCourse',5);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (6,'Pizza','Italian','mainCourse',10);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (7,'Schabowy','Polish','mainCourse',15);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (9,'Cola','Polish','drink',2);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (10,'Water','Polish','drink',1);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (11,'Ziemniaki','Polish','mainCourse',1);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (12,'Zupa grzybowa','Polish','mainCourse',2);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (13,'Vegetarian Pizza','Italian','mainCourse',5);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (14,'Lody truskawkowe','Polish','dessert',2);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (15,'Lody czekoladowe','Polish','dessert',2);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (16,'Lody wiśniowe','Polish','dessert',2);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (17,'Ciasto jabłkowe','Polish','dessert',3);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (18,'Italian cake','Italian','dessert',2);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (19,'Italian ice cream','Italian','dessert',3);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (20,'Tortilla with tomatoes','Mexican','mainCourse',7);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (21,'Tortilla with cheese','Mexican','mainCourse',8);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (22,'Tortilla with chili pepper','Mexican','mainCourse',9);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (23,'Nachos chips','Mexican','dessert',3);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (24,'Spaghetti','Italian','mainCourse',3);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (25,'Spaghetti with vegetables','Italian','mainCourse',4);
+INSERT INTO `dishes` (`id`,`dishname`,`cuisine`,`typeofmeal`,`price`) VALUES (26,'Italian Biscuits','Italian','dessert',1);
